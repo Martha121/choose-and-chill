8a19f968c441ceacdfb2ad1dd0da212ce3682076
Cooper <cooperflynn@utexas.edu>
2021-01-24 11:28:20 -0600

    more minor updates

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,7 +9,7 @@ if(urlParamsGenre){
 }
 
 //function to fetch titles by genre
-var getFilteredResults = function(genre) {
+function getFilteredResults(genre) {
 	console.log(genre);
 	var resultsUrl = "https://unogs-unogs-v1.p.rapidapi.com/aaapi.cgi?q=-!1900%2C2018-!0%2C5-!7%2C10-!" + genre + "-!Any-!Any-!Any-!gt100-!%7Bdownloadable%7D&t=ns&cl=78&st=adv&ob=Relevance&p=1&sa=and";
 	fetch(resultsUrl, {
@@ -31,8 +31,30 @@ var getFilteredResults = function(genre) {
 
 }
 
+//function to fetch title details by id
+function getDetailsById(netflixId) {
+	var detailsUrl = "https://unogs-unogs-v1.p.rapidapi.com/aaapi.cgi?t=loadvideo&q=" + netflixId;
+	fetch(detailsUrl, {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-key": "0c425d1814msh0b34ce4efb75316p1d5409jsn5681f63cb68d",
+		"x-rapidapi-host": "unogs-unogs-v1.p.rapidapi.com"
+	}
+})
+.then(response => {
+	response.json().then(function(data) {
+		console.log(data);
+		displayMovieDetails(data);
+		});
+})
+.catch(err => {
+	console.error(err);
+});
+
+}
+
 //convert search input to genre id
-var searchConverter = function(genre) {
+function searchConverter(genre) {
 	genre = genre.toLowerCase();
 	console.log(genre);
 	if(genre == "action") {
@@ -74,13 +96,12 @@ var displayNetflixResults = function(searchResults) {
 		var movieTitle = randomResults[i].title;
         var movieTitleButton = document.createElement("button");
 		movieTitleButton.textContent = randomResults[i].title;
-		movieTitleButton.value = 
+		movieTitleButton.value = randomResults[i].netflixid; 
 		movieTitleButton.addEventListener("click", function(event){
 			var targetElement = event.target;
-			movieTitleButtonClick(targetElement, movieTitle, movieImageSrc, movieSynopsis);
+			btnMovieTitleClick(targetElement);
 		})
         
-
 		var movieRowEl = document.getElementById('movie-row');
 		console.log(movieRowEl);
 		var movieColEl = document.createElement("div");
@@ -92,13 +113,33 @@ var displayNetflixResults = function(searchResults) {
 	}
 }
 
-var movieTitleButtonClick = function () {
-	
+var btnMovieTitleClick = function(targetElement) {
+	var netflixId = targetElement.value;
+	getDetailsById(netflixId);
 }
 
-var displayTitleDetails = function() {
+var displayMovieDetails = function(movie) {
+	var titleDetailsEl = document.getElementById("title-details");
+	var movieTitleEl = document.createElement("h2");
+	movieTitleEl.textContent = movie.RESULT.nfinfo.title;
+	var movieImageEl = document.createElement("img");
+	movieImageEl.src = movie.RESULT.nfinfo.image1;
+	var movieSynopsisEl = document.createElement("p");
+	movieSynopsisEl.textContent = movie.RESULT.nfinfo.synopsis;
+	var movieLinkEl = document.createElement("a");
+	var str1 = "https://www.netflix.com/browse?jbv=";
+	var str2 = movie.RESULT.nfinfo.netflixid;
+	movieLinkEl.href = str1.concat(str2);
+	movieLinkEl.textContent = "Link to Netflix Page";
 
+
+
+	titleDetailsEl.appendChild(movieTitleEl);
+	titleDetailsEl.appendChild(movieImageEl);
+	titleDetailsEl.appendChild(movieSynopsisEl);
+	titleDetailsEl.appendChild(movieLinkEl);
 }
 
-// TEST FUNCTION CALL
-searchConverter("comedy");
+function movieBtnGoClick(){
+	window.location.href = "movie-options.html?genre=drama";
+}
