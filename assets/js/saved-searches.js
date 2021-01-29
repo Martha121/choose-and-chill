9b701df3b287aa922738f157d1f105414faf2342
MGcommits <marthagamez211@gmail.com>
2021-01-29 16:19:31 -0600

    Code refactor

--- assets/js/saved-searches.js
+++ assets/js/saved-searches.js
@@ -55,7 +55,6 @@ document.addEventListener('DOMContentLoaded', function() {
                             <p class="recipe-title">` + result.title + `</p>
                             <div class="card-action">
                                 <a class="view-recipe" href="` + result.url + `" target="_blank">View Recipe</a>
-                                
                             </div>
                             <div class="card-action">
                                 <a class="remove-btn view-recipe" onclick="removeRecipeClick('food-` + result.title + `')">REMOVE RECIPE</a>
